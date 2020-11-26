007ea30b38133576ad787f757013062443b818c2
Tomos Slater <Tomos.Slater@softwire.com>
2020-11-26 09:21:31 +0000

    Main now runs the console application

--- src/main/java/training/metofficeweather/Main.java
+++ src/main/java/training/metofficeweather/Main.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-
+        ConsoleApplication app = new ConsoleApplication();
+        app.runApplication();
     }
 }
